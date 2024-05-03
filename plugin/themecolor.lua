9744eae98166e2def4bb7ee3ec1c4f4c02237d4e
MichaelShawUK <MichaelShawUK@gmail.com>
2024-05-03 12:25:25 +0100

    Cancel themecolor plugin load

--- plugin/themecolor.lua
+++ plugin/themecolor.lua
@@ -3,17 +3,17 @@
 -- If you want to programatically load Lua scripts then place the scripts in the lua dir in your
 -- runtimepath and include them with require '<filename>'
 
-vim.api.nvim_create_autocmd('FileType', {
-  group = vim.api.nvim_create_augroup('FileTypeColor', { clear = true }),
-  callback = function(ev)
-    if ev.match == 'html' then
-      require 'html_color'
-    end
-    if ev.match == 'css' then
-      require 'css_color'
-    end
-    if ev.match == 'javascript' then
-      require 'javascript_color'
-    end
-  end,
-})
+-- vim.api.nvim_create_autocmd('FileType', {
+--   group = vim.api.nvim_create_augroup('FileTypeColor', { clear = true }),
+--   callback = function(ev)
+--     if ev.match == 'html' then
+--       require 'html_color'
+--     end
+--     if ev.match == 'css' then
+--       require 'css_color'
+--     end
+--     if ev.match == 'javascript' then
+--       require 'javascript_color'
+--     end
+--   end,
+-- })
